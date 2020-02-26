2af141391b3db2b6a9308f7dd6f81b14e6bd29ea
yangzc <yzcorange@163.com>
2020-02-27 03:01:02 +0800

    框架搭建

---
+++ flutter_project/lib/pages/news_list_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NewsListPage extends StatefulWidget {
+  @override
+  _NewsListPageState createState() => _NewsListPageState();
+}
+
+class _NewsListPageState extends State<NewsListPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('资讯'),
+      ),
+    );
+  }
+}
